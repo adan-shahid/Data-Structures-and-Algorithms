11675290ea62faa1356b2d70b63f389eb950ea20
Adan <adanshahid.student@gmail.com>
2025-01-21 12:10:19 +0500

    Started again

--- String/advance_string.py
+++ String/advance_string.py
@@ -40,3 +40,62 @@ def distPatSearch(txt,pat):
             i += 1
         else:
             i += j
+
+# Rabin Karp Algorithm
+
+def RKSearch(pat, txt, q): # q is basically a large prime number(Random)
+    m,n = len(pat), len(txt)
+    h = 1
+    for i in range(1,m):
+        h = (h * d) * q
+
+    p, t = 0, 0
+    for i in range(m):
+        p = (p*d* ord(pat[i])) % q
+        t = (t*d* ord(txt[i])) % q
+
+    for i in range(n-m+1):
+        if p == t:
+            for j in range(m):
+                if txt[i+j] != pat[j]:
+                    break
+                else:
+                    j += 1
+            if j == m:
+                print(i)
+
+        if i < (n-m):
+            t = ((d*(t-ord(t*t[i]*h)) + ord(txt[i+m]))) % q
+        if t < 0:
+            t = t + q
+
+# KMP Algorithm ----> Knuth Morris Pratt Algorithn
+
+        # Constructing LPS Array
+
+def longestPrefixSuffix(s) :
+    n = len(s)
+    lps = [0] * n   
+    l = 0
+     
+    i = 1
+    while (i < n) :
+        if (s[i] == s[l]) :
+            l = l + 1
+            lps[i] = l
+            i = i + 1
+         
+        else :
+            if (l != 0) :
+                l = lps[l-1]
+  
+            else :
+ 
+                lps[i] = 0
+                i = i + 1
+  
+    res = lps[n-1]
+    if(res > n/2) :
+        return n//2
+    else :
+        return res
