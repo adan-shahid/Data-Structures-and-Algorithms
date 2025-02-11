2afddddf05b64a90e0686c102f545e264e07d5a3
Adan <adanshahid.student@gmail.com>
2025-02-11 22:05:34 +0500

    Started again, after wasting alot of my time Unfortunately

--- Stack/basic_stack.py
+++ Stack/basic_stack.py
@@ -67,4 +67,44 @@ def isMatching(a,b):
     else:
         return False
         
+# Implement 2 stacks in a array
+class twoStack:
+    def __init__(self,n):
+        self.size = n
+        self.arr = [None]*n
+        self.top1 = -1
+        self.top2 = self.size
+
+    def push1(self, x):
+        if self.top1 < self.top2-1: # this ensures that array has at least space for one item
+            self.top1 = self.top1 + 1
+            self.arr[self.top1] = x
+            return True
+        return False
+
+    def push2(self, x):
+        if self.top1 < self.top2 - 1:
+            self.top2 = self.top2 + 1
+            self[arr.top2] = x
+            return True
+        return False
+
+    def pop1(self):
+        if self.top1 >= 0:
+            x = self.arr[self.top1]
+            self.top1 = self.top1-1
+            return x
+        return None
+
+    def pop2(self):
+        if self.top2 < self.size:
+            x = self.arr[self.top2]
+            self.top2 = self.top2 + 1
+            return x
+        return None
+    def size1(self):
+        return self.top1 + 1
+    def size2(self):
+        return self.size - self.top2
+
 
