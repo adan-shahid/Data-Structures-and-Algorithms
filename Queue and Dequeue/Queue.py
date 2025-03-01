247eaa29a6d4d0c2e1c3bcf1c7f7fb45cf06ec28
Adan <adanshahid.student@gmail.com>
2025-03-01 21:49:04 +0500

    Completed queue portion

--- Queue and Dequeue/Queue.py
+++ Queue and Dequeue/Queue.py
@@ -38,4 +38,42 @@ class MyQueue:
             return res
         # End of class MyQueue 
 
+# Queue implementation using circular list
+class Myqueue:
+    def __init__(self,c):
+        self.l = [None]*c
+        self.cap = c
+        self.size = 0
+        self.front = 0
+    def getFront(self):
+        if self.size == 0:
+            return None
+        else:
+            return self.l[self.front]
+    def getRear(self):
+        if self.size == 0:
+            return None
+        else:
+            rear = (self.front + self.size-1) % self.cap
+            return self.l[rear]
+    def enque(self,x):
+        if self.size == self.cap:
+            return 
+        else:
+            rear = (self.front+self.size-1) % self.cap
+            rear = (rear+1) % self.cap
+            self.l[rear] = x
+            self.size = self.size + 1
+    def deque(self):
+        if self.size == 0:
+            return None
+        else:
+            res = self.l[self.front]
+            self.front = (self.front+1)%self.cap
+            self.size = self.size - 1
+            return res
+
+
+        
+
         
