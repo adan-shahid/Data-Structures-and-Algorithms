6363420c42e8367b6dd835786fb726ad2d9d17de
Adan <adanshahid.student@gmail.com>
2025-06-18 09:59:17 +0500

    Updated arrays

---
+++ gfg 160/array/secLargest.py
@@ -0,0 +1,19 @@
+# l = [1,34,56,87,43]
+# length = len(l)
+# i = 0
+# largest = 0
+# seclargest = 0
+# while (i < length):
+#   if l[i] > l[i + 1]:
+#     largest = l[i]
+#     seclargest = l[i+1]
+#   i = i + 1
+#   print(seclargest)
+# else:
+#   print(-1)
+
+
+i = 0
+while(i<5):
+  print(i)
+  i = i + 1
