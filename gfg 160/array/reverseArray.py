6e17db7e3eeaf81605725ae2850081e6a68eac1e
adan-shahid <adanshahid.student@gmail.com>
2025-07-10 08:22:03 +0500

    Reverse array

---
+++ gfg 160/array/reverseArray.py
@@ -0,0 +1,19 @@
+# 2 POINTER TECHNIQUE.
+# arr = [1,4,3,2, 6, 5]
+# left = 0
+# right = len(arr) - 1
+# while left < right:
+#     arr[left], arr[right] = arr[right], arr[left]
+#     left += 1
+#     right -= 1
+# print(arr)
+
+#BY SWAPPING
+# arr = [1,2,3,4,5,6]
+# n = len(arr)
+# for i in range(n//2):
+#     temp = arr[i]
+#     arr[i] = arr[n-i-1]
+#     arr[n-i-1] = temp
+# print(arr)
+
